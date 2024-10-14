8c665cb300eda50d65e25957e3c1eb19a645463d
G00394595@atu.ie <G00394595@atu.ie>
2024-10-14 13:08:12 +0100

    FirstDone

--- src/main/java/ie/atu/week5/customerapp/CustomerOrderController.java
+++ src/main/java/ie/atu/week5/customerapp/CustomerOrderController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -26,11 +27,17 @@ public class CustomerOrderController {
     public ResponseEntity<String> createCustomerWithOrders(@RequestBody CustomerOrderRequest customerOrderRequest) {
 
         // 1. Save the Customer and get the generated customer ID
-
+        if (customerOrderRequest.getCustomer().getId() != null) {
+            ArrayList<Order> tempOrder = new ArrayList<>(customerOrderRequest.getOrders());
+            orderRepository.saveAll(tempOrder);
+            return ResponseEntity.ok("Added the orders for customer for "  + customerOrderRequest.getCustomer().getName() + " " + tempOrder);
+        } else  {
+            customerRepository.save(customerOrderRequest.getCustomer());
+            ArrayList<Order> tempOrder = new ArrayList<>(customerOrderRequest.getOrders());
+            orderRepository.saveAll(tempOrder);
+            return ResponseEntity.ok("Added the orders for customer for "  + customerOrderRequest.getCustomer().getName() + " " + tempOrder);
+        }
 
         // 2. Save the Orders and link them to the customer
-
-
-        return ResponseEntity.ok("Customer and orders created successfully");
     }
 }
